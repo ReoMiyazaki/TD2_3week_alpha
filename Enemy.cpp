52fc0d0d3939af8b0122e7dae7cf571c703cd535
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-10-19 15:42:49 +0900

    Playerすり抜け解消

--- Enemy.cpp
+++ Enemy.cpp
@@ -40,7 +40,7 @@ void Enemy::Draw(ViewProjection viewProjection, int i)
 
 	model_->Draw(worldTransforms_[i], viewProjection, textureHandle_);
 	//	model_->Draw(worldTransform_, viewProjection, textureHandle_);
-	debugText_->SetPos(500, 70 + 35 * i);
+	debugText_->SetPos(50, 190 + 35 * i);
 	debugText_->Printf("isHit[%d] : %d", i, isHit);
 }
 
